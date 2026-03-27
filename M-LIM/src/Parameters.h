9518dd2930ba04dae6ca25faecf0c4ec3ce5e3f9
Swarm Worker 3 <worker-3@swarm>
2026-03-27 23:39:39 +0000

    worker-3: complete task 473 - change ParamID constants from juce::String to constexpr const char*

--- M-LIM/src/Parameters.h
+++ M-LIM/src/Parameters.h
@@ -5,28 +5,28 @@
 // Parameter ID constants
 namespace ParamID
 {
-    inline const juce::String inputGain       = "inputGain";
-    inline const juce::String outputCeiling   = "outputCeiling";
-    inline const juce::String algorithm       = "algorithm";
-    inline const juce::String lookahead       = "lookahead";
-    inline const juce::String attack          = "attack";
-    inline const juce::String release         = "release";
-    inline const juce::String channelLinkTransients = "channelLinkTransients";
-    inline const juce::String channelLinkRelease    = "channelLinkRelease";
-    inline const juce::String truePeakEnabled  = "truePeakEnabled";
-    inline const juce::String oversamplingFactor = "oversamplingFactor";
-    inline const juce::String dcFilterEnabled  = "dcFilterEnabled";
-    inline const juce::String ditherEnabled    = "ditherEnabled";
-    inline const juce::String ditherBitDepth   = "ditherBitDepth";
-    inline const juce::String ditherNoiseShaping = "ditherNoiseShaping";
-    inline const juce::String bypass           = "bypass";
-    inline const juce::String unityGainMode    = "unityGainMode";
-    inline const juce::String sidechainHPFreq  = "sidechainHPFreq";
-    inline const juce::String sidechainLPFreq  = "sidechainLPFreq";
-    inline const juce::String sidechainTilt    = "sidechainTilt";
-    inline const juce::String delta            = "delta";
-    inline const juce::String displayMode      = "displayMode";
-    inline const juce::String loudnessTarget   = "loudnessTarget";
+    constexpr const char* inputGain       = "inputGain";
+    constexpr const char* outputCeiling   = "outputCeiling";
+    constexpr const char* algorithm       = "algorithm";
+    constexpr const char* lookahead       = "lookahead";
+    constexpr const char* attack          = "attack";
+    constexpr const char* release         = "release";
+    constexpr const char* channelLinkTransients = "channelLinkTransients";
+    constexpr const char* channelLinkRelease    = "channelLinkRelease";
+    constexpr const char* truePeakEnabled  = "truePeakEnabled";
+    constexpr const char* oversamplingFactor = "oversamplingFactor";
+    constexpr const char* dcFilterEnabled  = "dcFilterEnabled";
+    constexpr const char* ditherEnabled    = "ditherEnabled";
+    constexpr const char* ditherBitDepth   = "ditherBitDepth";
+    constexpr const char* ditherNoiseShaping = "ditherNoiseShaping";
+    constexpr const char* bypass           = "bypass";
+    constexpr const char* unityGainMode    = "unityGainMode";
+    constexpr const char* sidechainHPFreq  = "sidechainHPFreq";
+    constexpr const char* sidechainLPFreq  = "sidechainLPFreq";
+    constexpr const char* sidechainTilt    = "sidechainTilt";
+    constexpr const char* delta            = "delta";
+    constexpr const char* displayMode      = "displayMode";
+    constexpr const char* loudnessTarget   = "loudnessTarget";
 }
 
 juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
